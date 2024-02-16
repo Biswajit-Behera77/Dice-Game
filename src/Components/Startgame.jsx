46d88146d183952022090247a08c0989677fb57c
Your Name <you@example.com>
2024-02-16 10:51:26 +0530

    name change

--- src/Components/Startgame.jsx
+++ src/Components/Startgame.jsx
@@ -7,7 +7,7 @@ const Startgame=({toggle}) => {
         <img src="/images/dicess.png" alt="main pic" />
         </div>
         <div className='Content'>
-            <h1>No Dice game</h1>
+            <h1>Dice game</h1>
              <Button onClick={toggle}>Play Now</Button>
         </div>
      </Container>
